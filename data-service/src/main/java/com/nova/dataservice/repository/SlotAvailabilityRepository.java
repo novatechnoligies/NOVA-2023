d7a50c4605cf1d1eb38bc5624abccf3ecbcb25b5
raviroogi5 <49854390+raviroogi5@users.noreply.github.com>
2023-05-19 22:53:57 +0530

    Error Fixed

--- data-service/src/main/java/com/nova/dataservice/repository/SlotAvailabilityRepository.java
+++ data-service/src/main/java/com/nova/dataservice/repository/SlotAvailabilityRepository.java
@@ -2,7 +2,9 @@ package com.nova.dataservice.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import com.nova.dataservice.entity.ShopAvalibility;
 @Repository
-public interface SlotAvailabilityRepository extends JpaRepository<SlotAvailability, Long> {
+public interface SlotAvailabilityRepository extends JpaRepository<ShopAvalibility, Long> {
 
 }
--- data-service/src/main/java/com/nova/dataservice/repository/UserDetailsRepository.java
+++ data-service/src/main/java/com/nova/dataservice/repository/UserDetailsRepository.java
@@ -3,6 +3,8 @@ package com.nova.dataservice.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import com.nova.dataservice.entity.UserDetails;
+
 @Repository
 public interface UserDetailsRepository extends JpaRepository<UserDetails, Long> {
 
