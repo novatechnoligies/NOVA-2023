a3e7ddc4c8b3a2b8e756d5aa795c55829c3feb5d
raviroogi5 <49854390+raviroogi5@users.noreply.github.com>
2023-05-20 01:08:01 +0530

    servicerelation and name changed

--- data-service/src/main/java/com/nova/dataservice/controller/ShopServiceRelationController.java
+++ data-service/src/main/java/com/nova/dataservice/controller/ShopServiceRelationController.java
@@ -1,8 +1,33 @@
 package com.nova.dataservice.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.nova.dataservice.entity.ShopServiceRelation;
+import com.nova.dataservice.service.ShopServiceRelationServices;
 
 @RestController
 class ShopServiceRelationController {
+	
+	@Autowired
+	ShopServiceRelationServices relationServices;
+	
+	@PostMapping(value = "saveShopServiceRelation")
+	public ResponseEntity<Object> saveShopServiceRelation(@RequestBody ShopServiceRelation serviceRelation) {
+		try {
+			ShopServiceRelation data =	relationServices.saveShopServiceRelation(serviceRelation);
+			if (data!=null) {
+				return new ResponseEntity<Object>(data,HttpStatus.OK);
+			} else {
+				return new ResponseEntity<Object>("data not saved",HttpStatus.OK);
+			}
+		} catch (Exception e) {
+			return new ResponseEntity<Object>("somthing went wrong",HttpStatus.OK);
+		}
+	}
 
 }
--- data-service/src/main/java/com/nova/dataservice/service/ShopServiceRelationServices.java
+++ data-service/src/main/java/com/nova/dataservice/service/ShopServiceRelationServices.java
@@ -1,5 +1,9 @@
 package com.nova.dataservice.service;
 
+import com.nova.dataservice.entity.ShopServiceRelation;
+
 public interface ShopServiceRelationServices {
+
+	ShopServiceRelation saveShopServiceRelation(ShopServiceRelation serviceRelation);
 
 }
