b29dcf130bb00d985c07dd6f620e7f6e73b04748
raviroogi5 <49854390+raviroogi5@users.noreply.github.com>
2023-05-20 01:23:22 +0530

    shopType

--- data-service/src/main/java/com/nova/dataservice/controller/ShopTypeController.java
+++ data-service/src/main/java/com/nova/dataservice/controller/ShopTypeController.java
@@ -1,8 +1,32 @@
 package com.nova.dataservice.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.nova.dataservice.entity.ShopType;
+import com.nova.dataservice.service.ShopTypeServices;
 
 @RestController
 public class ShopTypeController {
+	@Autowired
+	ShopTypeServices services;
+	@PostMapping(value="saevShopType")
+	public ResponseEntity<Object> saveShopType(@RequestBody ShopType shopType) {
+		try {
+			ShopType data =	services.saveShopType(shopType);
+			if (data!=null) {
+				return new ResponseEntity<Object>(data, HttpStatus.OK);
+			} else {
+				return new ResponseEntity<Object>("failed", HttpStatus.OK);
+			}
+		} catch (Exception e) {
+			return new ResponseEntity<Object>("somthing went wrong", HttpStatus.OK);
+		}
+	}
 
 }
