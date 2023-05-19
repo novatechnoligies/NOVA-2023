9ee343e9c9a55af30fbb0c3eae0aa84d9a150484
raviroogi5 <49854390+raviroogi5@users.noreply.github.com>
2023-05-19 23:21:33 +0530

    CRUD for appontment

---
+++ data-service/src/main/java/com/nova/dataservice/controller/AppointmentDetailsController.java
@@ -0,0 +1,69 @@
+package com.nova.dataservice.controller;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.nova.dataservice.entity.AppontmentDetails;
+import com.nova.dataservice.service.AppointmentDetailservice;
+
+
+@RestController
+public class AppointmentDetailsController {
+	
+	@Autowired
+	AppointmentDetailservice appService;
+	
+	@PostMapping(value = "saveAppointment")
+	public ResponseEntity<Object> saveAppointment(@RequestBody AppontmentDetails rl) {
+		try {
+			AppontmentDetails data = appService.saveAppointment(rl);
+			if (data!=null) {
+				return new ResponseEntity<>(data, HttpStatus.OK);
+			}else {
+				return new ResponseEntity<>("fail to save data", HttpStatus.OK);
+			}
+		} catch (Exception e) {
+			return new ResponseEntity<>("somthing went wrong fail to save data , due to MYSQL is down", HttpStatus.OK);
+		}
+	}
+	
+	@GetMapping(value = "/getAllAppointment")
+	public ResponseEntity<Object> getAllAppontments() {
+		try {
+			List<AppontmentDetails> appList =	appService.findAllAppointment();
+			if (appList!=null && !appList.isEmpty()) {
+				return new ResponseEntity<>(appList, HttpStatus.OK);
+			}else {
+				return new ResponseEntity<>("fail to get data", HttpStatus.OK);
+			}
+		} catch (Exception e) {
+			return new ResponseEntity<>("somthing went wrong fail to get data , due to MYSQL is down", HttpStatus.OK);
+		}
+	
+	}
+	
+	@GetMapping(value = "/getAllAppointment/{appId}")
+	public ResponseEntity<Object> getAllAppontments(@PathVariable("appId") Long id) {
+		try {
+			Optional<AppontmentDetails> app =	appService.findAppointmentById(id);
+			if (app!=null && !app.isEmpty()) {
+				return new ResponseEntity<>(app, HttpStatus.OK);
+			}else {
+				return new ResponseEntity<>("fail to get data", HttpStatus.OK);
+			}
+		} catch (Exception e) {
+			return new ResponseEntity<>("somthing went wrong fail to get data , due to MYSQL is down", HttpStatus.OK);
+		}
+	
+	}
+
+}
--- data-service/src/main/java/com/nova/dataservice/serviceImpl/AppointmentDetailsserviceimpl.java
+++ data-service/src/main/java/com/nova/dataservice/serviceImpl/AppointmentDetailsserviceimpl.java
@@ -1,10 +1,37 @@
 package com.nova.dataservice.serviceImpl;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.nova.dataservice.entity.AppontmentDetails;
+import com.nova.dataservice.repository.AppointmentDetailsRepository;
 import com.nova.dataservice.service.AppointmentDetailservice;
 
 @Service
 public class AppointmentDetailsserviceimpl implements AppointmentDetailservice{
+
+	@Autowired
+	AppointmentDetailsRepository appRepo;
+	
+	@Override
+	public AppontmentDetails saveAppointment(AppontmentDetails rl) {
+		// TODO Auto-generated method stub
+		return appRepo.save(rl);
+	}
+
+	@Override
+	public List<AppontmentDetails> findAllAppointment() {
+		// TODO Auto-generated method stub
+		return appRepo.findAll();
+	}
+
+	@Override
+	public Optional<AppontmentDetails> findAppointmentById(Long id) {
+		// TODO Auto-generated method stub
+		return appRepo.findById(id);
+	}
 
 }
