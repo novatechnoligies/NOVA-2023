ff9c40881b0b375d614b7aa44a9999453102a173
raviroogi5 <49854390+raviroogi5@users.noreply.github.com>
2023-05-21 09:40:07 +0530

    shopServiceRelationCode Updated

--- data-service/src/main/java/com/nova/dataservice/service/ShopServiceRelationServices.java
+++ data-service/src/main/java/com/nova/dataservice/service/ShopServiceRelationServices.java
@@ -1,9 +1,16 @@
 package com.nova.dataservice.service;
 
+import java.util.List;
+import java.util.Optional;
+
 import com.nova.dataservice.entity.ShopServiceRelation;
 
 public interface ShopServiceRelationServices {
 
 	ShopServiceRelation saveShopServiceRelation(ShopServiceRelation serviceRelation);
+
+	List<ShopServiceRelation> findAllShopServiceRelation();
+
+	Optional<ShopServiceRelation> shopServiceRelationfindById(long id);
 
 }
