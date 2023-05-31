4a40e2e0ea7b0645f34f36db887f68c3c27035c7
raviroogi5 <49854390+raviroogi5@users.noreply.github.com>
2023-05-31 15:39:05 +0530

    Update UserDetails.java

--- data-service/src/main/java/com/nova/dataservice/entity/UserDetails.java
+++ data-service/src/main/java/com/nova/dataservice/entity/UserDetails.java
@@ -23,17 +23,29 @@ public class UserDetails {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Column(name = "id")
 	private Long id;
+	@Column(name = "first_name")
 	private String firstName;
+	@Column(name = "last_name")
 	private String lastName;
+	@Column(name = "email")
 	private String email;
+	@Column(name = "phone")
 	private String phone;
+	@Column(name = "pin")
 	private String pin;
+	@Column(name = "gender")
 	private String gender;
+	@Column(name = "created_at")
 	private LocalDate createdAt;
+	@Column(name = "status")
 	private Boolean status;
+	@Column(name = "is_deleted")
 	private Boolean isDeleted;
+	@Column(name = "owner_id")
 	private Long ownerId;
+	@Column(name = "username")
 	private String username;
+	@Column(name = "password")
 	private String password;
 	@ManyToOne(fetch = FetchType.LAZY)
 	@JoinColumn(name = "role_id")
@@ -42,10 +54,127 @@ public class UserDetails {
 	@OneToMany(mappedBy = "owner")
 	private List<ShopDetails> shops = new ArrayList<>();
 
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	public String getPin() {
+		return pin;
+	}
+
+	public void setPin(String pin) {
+		this.pin = pin;
+	}
+
+	public String getGender() {
+		return gender;
+	}
+
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+
+	public LocalDate getCreatedAt() {
+		return createdAt;
+	}
+
+	public void setCreatedAt(LocalDate createdAt) {
+		this.createdAt = createdAt;
+	}
+
+	public Boolean getStatus() {
+		return status;
+	}
+
+	public void setStatus(Boolean status) {
+		this.status = status;
+	}
+
+	public Boolean getIsDeleted() {
+		return isDeleted;
+	}
+
+	public void setIsDeleted(Boolean isDeleted) {
+		this.isDeleted = isDeleted;
+	}
+
+	public Long getOwnerId() {
+		return ownerId;
+	}
+
+	public void setOwnerId(Long ownerId) {
+		this.ownerId = ownerId;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public Role getRole() {
+		return role;
+	}
+
+	public void setRole(Role role) {
+		this.role = role;
+	}
+
+	public List<ShopDetails> getShops() {
+		return shops;
+	}
+
+	public void setShops(List<ShopDetails> shops) {
+		this.shops = shops;
+	}
+
 //	@OneToMany(mappedBy = "shop")
 //	private List<SlotAvailability> timeAvailabilities = new ArrayList<>();
-
-	
-	
 
 }
