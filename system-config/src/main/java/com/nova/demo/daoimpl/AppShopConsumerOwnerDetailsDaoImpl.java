017c5fb7800b5a6a51f2d1e31ded768ac6e81c7f
Santosh <78397565+santoshsh2307@users.noreply.github.com>
2023-07-08 11:43:26 +0530

    remove

--- system-config/src/main/java/com/nova/demo/daoimpl/AppShopConsumerOwnerDetailsDaoImpl.java
+++ system-config/src/main/java/com/nova/demo/daoimpl/AppShopConsumerOwnerDetailsDaoImpl.java
@@ -11,7 +11,6 @@ import java.util.Optional;
 import org.hibernate.query.NativeQuery;
 import org.hibernate.transform.Transformers;
 import org.hibernate.type.StandardBasicTypes;
-import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
