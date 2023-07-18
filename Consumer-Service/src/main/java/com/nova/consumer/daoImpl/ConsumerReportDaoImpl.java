992349468d05b2fbe61d2b3d893b0e126e19988b
Mahamad Ruhan <darshanr656@gmail.com>
2023-07-18 21:34:17 +0530

    API for "getServiceDetailsByServiceName" and "findAllServicesByShopName"

--- Consumer-Service/src/main/java/com/nova/consumer/daoImpl/ConsumerReportDaoImpl.java
+++ Consumer-Service/src/main/java/com/nova/consumer/daoImpl/ConsumerReportDaoImpl.java
@@ -1,11 +1,17 @@
 package com.nova.consumer.daoImpl;
 
+
+import java.sql.Date;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
 import javax.persistence.EntityManager;
 import javax.persistence.Query;
 
+import org.hibernate.query.NativeQuery;
+import org.hibernate.transform.Transformers;
+import org.hibernate.type.StandardBasicTypes;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
@@ -14,6 +20,7 @@ import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.nova.consumer.dao.ConsumerReportDao;
 import com.nova.consumer.dto.ConsumerReportDTO;
+import com.nova.consumer.dto.ServiceDetailDTO;
 
 @Repository
 public class ConsumerReportDaoImpl implements ConsumerReportDao {
@@ -49,4 +56,61 @@ public class ConsumerReportDaoImpl implements ConsumerReportDao {
 			
 	}
 
+	@Override
+	public List<ServiceDetailDTO> getServiceDetailsByServiceName(String serviceName) {
+		String sql = "select sd.shop_name as shopName,ssr.amount as amount, ssr.shop_id as shopId, ssr.service_id as serviceId, sm.name as serviceName from service_master as sm"
+				+ " join shop_service_relation as ssr on ssr.service_id=sm.id"
+				+ " join shop_details as sd on sd.id=ssr.shop_id"
+				+ " where sm.name LIKE :serviceName";
+		
+		Query query = entityManager.createNativeQuery(sql.toString())
+				.setParameter("serviceName", "%" + serviceName + "%");
+		
+		query.unwrap(NativeQuery.class)
+		.addScalar("shopName",StandardBasicTypes.STRING)
+        .addScalar("amount", StandardBasicTypes.FLOAT)
+        .addScalar("shopId", StandardBasicTypes.LONG)
+        .addScalar("serviceId", StandardBasicTypes.LONG)
+        .addScalar("serviceName", StandardBasicTypes.STRING);
+		
+        ((NativeQuery) query).setResultTransformer(Transformers.aliasToBean(ServiceDetailDTO.class));
+        
+        Object result = query.getResultList();
+        
+        if (result != null) {
+			return(List<ServiceDetailDTO>)result;
+		}else {
+			return null;
+		}
+	
+	}
+
+	@Override
+	public List<ServiceDetailDTO> findAllServicesByShopName(String shopName) {
+		String sql = "SELECT sd.shop_name AS shopName, ssr.amount AS amount, ssr.shop_id AS shopId, ssr.service_id AS serviceId, sm.name AS serviceName "
+	            + "FROM service_master AS sm "
+	            + "JOIN shop_service_relation AS ssr ON ssr.service_id = sm.id "
+	            + "JOIN shop_details AS sd ON sd.id = ssr.shop_id "
+	            + "WHERE sd.shop_name LIKE :shopName";
+		
+		Query query = entityManager.createNativeQuery(sql.toString())
+			.setParameter("shopName", "%" + shopName + "%");
+			
+			query.unwrap(NativeQuery.class)
+			.addScalar("shopName",StandardBasicTypes.STRING)
+	        .addScalar("amount", StandardBasicTypes.FLOAT)
+	        .addScalar("shopId", StandardBasicTypes.LONG)
+	        .addScalar("serviceId", StandardBasicTypes.LONG)
+	        .addScalar("serviceName", StandardBasicTypes.STRING);
+		
+			((NativeQuery) query).setResultTransformer(Transformers.aliasToBean(ServiceDetailDTO.class));
+	        
+	        Object result = query.getResultList();
+	        
+	        if (result != null) {
+				return(List<ServiceDetailDTO>)result;
+			}else {
+				return null;
+		}
+	}
 }
