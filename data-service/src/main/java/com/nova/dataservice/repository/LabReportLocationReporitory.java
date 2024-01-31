f0c2acaa76c2e71da890467471bfba1a38d7ccfd
kamalahadapadbh <85490665+kamalahadapadbh@users.noreply.github.com>
2024-01-31 00:49:00 -0800

    NOVACARD-141
    GetAllReportsByPatientIdAndLabIdAndCreatedAt

--- data-service/src/main/java/com/nova/dataservice/repository/LabReportLocationReporitory.java
+++ data-service/src/main/java/com/nova/dataservice/repository/LabReportLocationReporitory.java
@@ -1,5 +1,8 @@
 package com.nova.dataservice.repository;
 
+import java.time.LocalDate;
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import org.springframework.web.multipart.MultipartFile;
@@ -8,6 +11,9 @@ import com.nova.dataservice.entity.LabReportLocatiosn;
 
 @Repository
 public interface LabReportLocationReporitory extends JpaRepository<LabReportLocatiosn, Long>{
+
+	List<LabReportLocatiosn> findAllReportsByPatientIdAndLabIdAndCreatedAt(Long patientId, Long labId,
+			LocalDate createdAt);
 
 
 }
