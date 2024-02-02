4760758d2c582286d2fc1974ab0f689fd83675ad
kamalahadapadbh <85490665+kamalahadapadbh@users.noreply.github.com>
2024-02-02 05:24:30 -0800

    updated service price NOVACARD-147

--- data-service/src/main/java/com/nova/dataservice/controller/ShopServiceRelationController.java
+++ data-service/src/main/java/com/nova/dataservice/controller/ShopServiceRelationController.java
@@ -1,5 +1,6 @@
 package com.nova.dataservice.controller;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Optional;
 
@@ -8,8 +9,10 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,6 +21,7 @@ import com.nova.dataservice.DTO.ServiceDetailDTO;
 import com.nova.dataservice.DTO.ServiceMasterShopRelationDTO;
 import com.nova.dataservice.DTO.ServiceMasterShoplistDto;
 import com.nova.dataservice.entity.ServiceMasterShopRelation;
+import com.nova.dataservice.entity.ShopDetails;
 import com.nova.dataservice.service.ShopServiceRelationServices;
 
 @RestController
@@ -134,6 +138,25 @@ class ShopServiceRelationController {
 		} catch (Exception e) {
 			e.printStackTrace();
 			return new ResponseEntity<Object>("data not found", HttpStatus.OK);
+		}
+	}
+	@PatchMapping(value = "updatedServicePrice")
+	public ResponseEntity<Object> updatedServicePrice(Long serviceId, Float newPrice) {
+		try {
+			ServiceMasterShopRelation data = relationServices.updatedServicePrice(serviceId,newPrice);
+			if (data != null) {
+				HashMap<String, String> map = new HashMap<>();
+				map.put("200", "success");
+				map.put("message", "updated");
+				return new ResponseEntity<Object>(map, HttpStatus.OK);
+			} else {
+				return new ResponseEntity<Object>("ServicePrice Not Found", HttpStatus.OK);
+			}
+
+		} catch (Exception e) {
+			e.printStackTrace();
+			// TODO: handle exception
+			return new ResponseEntity<Object>("Something went wrong", HttpStatus.OK);
 		}
 	}
 
