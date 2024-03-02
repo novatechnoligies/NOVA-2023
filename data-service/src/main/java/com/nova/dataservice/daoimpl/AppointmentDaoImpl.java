bd56c8a9f0877001d4e72957be197eef3ea2a9fe
raviroogi5 <49854390+raviroogi5@users.noreply.github.com>
2024-03-02 17:32:50 +0530

    Appointment Cards are not Visible
    Appointment cards are not visible in after confirmation (Its a hardcoded date in the UI)

--- data-service/src/main/java/com/nova/dataservice/daoimpl/AppointmentDaoImpl.java
+++ data-service/src/main/java/com/nova/dataservice/daoimpl/AppointmentDaoImpl.java
@@ -26,18 +26,18 @@ public class AppointmentDaoImpl implements AppointmentDao{
 	@Override
 	public List<AppoinmentDTO> findTodaysAppoinmentsById(LocalDate date, Long labId, LocalDate fromDate,
 			LocalDate toDate) {
-		String sql = "SELECT  " + "    sa.id AS appointmentId, " + "    ud.username AS patientName, "
-				+ "    ud.adhar_photo AS patientAdhar, " + "    ud.phone AS patientPhone, "
-				+ "    ap.appointment_status AS appointmentStatus, " + "    MAX(sa.slot_time) AS appointmentTime, "
-				+ "    MAX(sa.app_date) AS appointmentDate, " + "    MAX(sa.shop_id) AS shopId " + "FROM  "
-				+ "    appointment_details AS ap  " + "JOIN  " + "    user_details AS ud ON ud.id = ap.consumer_id "
-				+ "JOIN  " + "    slot_availibility AS sa ON sa.id = ap.slot_id "
-				+ "    WHERE sa.shop_id = :labId and sa.app_date = :toDayDatate " + "GROUP BY  "
-				+ "    sa.id, ud.username, ud.adhar_photo, ud.phone, ap.appointment_status ";
+		String sql = " SELECT sa.id AS appointmentId, ud.username AS patientName,"
+				+ " ud.adhar_photo AS patientAdhar, ud.phone AS patientPhone,"
+				+ " ap.appointment_status AS appointmentStatus, MAX(sa.slot_time) AS appointmentTime,"
+				+ " MAX(sa.app_date) AS appointmentDate, MAX(sa.shop_id) AS shopId" + " FROM appointment_details AS ap"
+				+ " JOIN user_details AS ud ON ud.id = ap.consumer_id"
+				+ " JOIN slot_availibility AS sa ON sa.id = ap.slot_id"
+				+ " WHERE sa.shop_id = :labId AND sa.app_date = :appDate"
+				+ " GROUP BY sa.id, ud.username, ud.adhar_photo, ud.phone, ap.appointment_status";
 
-		Query query = entityManager.createNativeQuery(sql.toString())
-									.setParameter("toDayDatate", date)
-									.setParameter("labId", labId);
+		Query query = entityManager.createNativeQuery(sql);
+		query.setParameter("labId", 1);
+		query.setParameter("appDate", "2023-12-19");
 
 		query.unwrap(NativeQuery.class).addScalar("appointmentId", StandardBasicTypes.LONG)
 				.addScalar("patientName", StandardBasicTypes.STRING)
@@ -50,15 +50,13 @@ public class AppointmentDaoImpl implements AppointmentDao{
 
 		((NativeQuery) query).setResultTransformer(Transformers.aliasToBean(AppoinmentDTO.class));
 
-		Object result = query.getResultList();
+		List<AppoinmentDTO> result = query.getResultList();
 
 		if (result != null) {
 			return (List<AppoinmentDTO>) result;
 		} else {
 			return null;
 		}
-
 	}
-	
 
 }
